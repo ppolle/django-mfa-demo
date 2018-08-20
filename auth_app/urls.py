7e9a7205f696c6793ee89fb996e3fdfec99638f0
ppolle <peter.m.polle@gmail.com>
2018-08-20 19:41:48 +0300

    this commit will implement a signup functionality

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('^sign-up$',views.signup, name = 'signup'),
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def welcome(request):
@@ -6,3 +8,20 @@ def welcome(request):
 	View function to render the landing page
 	'''
 	return render(request,'index.html')
+
+def signup(request):
+	'''
+	View function to handle signup functionality
+	'''
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username =  form.cleaned_data.get('username')
+			password  =  form.cleaned_data.get('password1')
+			user = authenticate(username = username,password=password)
+			return redirect('login')
+
+	else:
+		form = UserCreationForm()
+		return render(request,'authentication/signup.html',{'form':form})
