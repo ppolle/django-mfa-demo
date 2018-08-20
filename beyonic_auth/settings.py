c611853b7a27efd5538e1b694420ba8ec2f0de06
ppolle <peter.m.polle@gmail.com>
2018-08-20 17:04:24 +0300

    this commit will create a navbar partial

--- beyonic_auth/settings.py
+++ beyonic_auth/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'beyonic_auth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'beyonic',
+        'USER': 'peter',
+        'PASSWORD': 'iamBOSS12',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
