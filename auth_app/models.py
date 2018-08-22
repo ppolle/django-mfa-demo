5b79a67dc0d91e49f287eb093aa06d5a2147c033
ppolle <peter.m.polle@gmail.com>
2018-08-22 14:15:51 +0300

    this commit will create a profile model to handle email tokens invalidation and google_auth tokn validations

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_confirmed = models.BooleanField(default=False)
+    google_auth_enabled = models.BooleanField(default=False)
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
