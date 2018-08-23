f5397e0b943354e8cb5eb7b2adc72f851081d0cb
ppolle <peter.m.polle@gmail.com>
2018-08-23 17:50:54 +0300

    this commit will create a new nexmo_auth app to handle user verification with nexmo's sms sfa authentication gateway

--- beyonic_auth/settings.py
+++ beyonic_auth/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'auth_app',
+    'nexmo_auth',
     'bootstrap3',
     'qr_code',
     
---
+++ nexmo_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NexmoAuthConfig(AppConfig):
+    name = 'nexmo_auth'
